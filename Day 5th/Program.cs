d1bffc94eb06a638e3a86d1d2f63acca43357568
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-23 09:58:35 +0100

    Add project files.

---
+++ Day 5th/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] grades = int[365];
+        String[] daysofweek = new String[7];
+        daysofweek[0] = "Poniedziałek";
+        daysofweek[1] = "Wtorek";
+        daysofweek[2] = "Środa";
+        daysofweek[3] = "Czwartek";
+        daysofweek[4] = "Piątek";
+        daysofweek[5] = "Sobota";
+        daysofweek[6] = "Niedziela";
+
+        
+
+    }
+}
